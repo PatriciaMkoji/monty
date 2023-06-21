5c46e7a32ff80bdf86e2a0912cb85e8545867bd5
PatriciaMkoji <keyshotrisha@gmail.com>
2023-06-21 05:45:53 -0700

    Monty project attempt

--- monty.h
+++ monty.h
@@ -34,8 +34,6 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
 
 
 
